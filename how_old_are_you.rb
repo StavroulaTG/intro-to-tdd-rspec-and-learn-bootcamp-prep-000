68fb5ea73d360db50aac61eb29b061198961ec75
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-21 16:57:53 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,6 +1,6 @@
-def touch how_old_are_you.rb
+ touch how_old_are_you.rb
 
-require_relative './current_age_for_birth_year.rb'
+def require_relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
